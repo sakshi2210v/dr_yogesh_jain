b531e37cdd0c2eace05e3c675aefad07071f118e
Itzfaizan06 <160049730+Itzfaizan06@users.noreply.github.com>
2024-10-15 00:26:34 +0530

    meta title

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,6 +5,14 @@ import Image from "next/image";
 import React from "react";
 import img from '@/assets/gallery.png'
 import Info from "@/components/Info";
+
+export const metadata = {
+  title: 'Contact Dr. Yogesh Jain for Professional Gallery Insights',
+  icons: {
+    icon: '/logo.jpg',
+  },
+  description: 'Get in touch with Dr. Yogesh Jain to learn more about his gallery and services. Reach out today for inquiries related to your health and wellness needs.',
+}
 const page = () => {
   return (
     <div>
--- src/app/treatment/page.jsx
+++ src/app/treatment/page.jsx
@@ -3,6 +3,14 @@ import Reviews from '@/components/Reviews'
 import Treatment from '@/components/Treatment'
 import React from 'react'
 
+export const metadata = {
+  title: 'Best Physician in Mumbai | Dr. Yogesh Jain Expert Medical Care',
+  icons: {
+    icon: '/logo.jpg',
+  },
+  description: 'Dr. Yogesh Jain, one of the best physicians in Mumbai, provides expert diagnosis and personalized treatment. Trust in his experience for comprehensive healthcare solutions.',
+}
+
 const page = () => {
   return (
     <div>
