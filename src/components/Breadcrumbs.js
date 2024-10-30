8d641e69bf11bd484945ac080e826218f035ceeb
Aditya Narkar <narkaraditya10@gmail.com>
2024-10-30 12:02:57 +0530

    updated packages

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -7,7 +7,7 @@ const BreadCrumbs = ({ breadCrumbs }) => {
       <div className="container max-w-screen-xl mx-auto px-4">
         <ol className="inline-flex flex-wrap text-gray-600 space-x-0 md:space-x-0 items-center">
           {breadCrumbs?.map((breadCrumb, index) => (
-            <li className="inline-flex items-center">
+            <li key={index} className="inline-flex items-center">
               <Link
                 href={breadCrumb.url}
                 className="text-gray-600 hover:text-blue-600"
